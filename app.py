d41982ae63d14f9d437a04843843631c088af49c
Rucha-PM <ruchakulkarni2990@gmail.com>
2025-05-21 13:50:12 +0530

    Update bot to ask user what they did today

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 @app.route("/whatsapp", methods=['POST'])
 def whatsapp_reply():
     print("✅ Twilio reached the Flask bot!")
+
     incoming_msg = request.values.get('Body', '').strip().lower()
     resp = MessagingResponse()
     msg = resp.message()
 
-    if 'done' in incoming_msg:
-        msg.body("Yay! You crushed it today 🎯")
-    else:
-        msg.body("Hi! I'm DanaDone. Just reply with 'done' when you complete your habit.")
+    msg.body("Hi! I am DanaDone! Your Daily Update Bot. What did you do today that you want to count or keep track of?")
 
     return str(resp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
+
